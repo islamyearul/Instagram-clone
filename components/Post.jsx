9c537193351ab144562035a7d1cb8fef9ec676ea
islamyearul <yearulislamonem@gmail.com>
2022-06-25 17:22:07 +0600

    Comment design

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,9 +1,9 @@
-import { BookmarkIcon, ChatIcon, DotsHorizontalIcon, HeartIcon } from '@heroicons/react/outline'
+import { BookmarkIcon, ChatIcon, DotsHorizontalIcon, EmojiHappyIcon, HeartIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 export default function Post({id, username, userimage, img, caption} ) {
   return (
-    <div className='bg-white my-7 border rounded-md '>
+    <div className='bg-white my-7 border rounded-md pb-5'>
         {/* post header */}
         <div className=" flex items-center p-5">
             <img className='h-12 rounded-full object-cover border p-1 mr-3' src={userimage} alt={username} />
@@ -21,6 +21,14 @@ export default function Post({id, username, userimage, img, caption} ) {
             </div>
             <BookmarkIcon className='btn' />
         </div>
+        {/* Post comments */}
+        <p className='p-5 truncate'><span className='font-bold mr-2 '>{username}</span> {caption}</p>
+        {/* post input box */}
+        <form   className='flex items-center p-4'>
+            <EmojiHappyIcon className='h-7' />
+            <input className='border-none flex-1 focus:rign-0 mx-5'  type="text" placeholder='Enter Your Comment' />
+            <button className='text-blue-400 font-bold' >Post </button>
+        </form>
     </div>
   )
 }
