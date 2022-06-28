8ee7a70b34fc01ee0b80351807a87012afb05c99
islamyearul <yearulislamonem@gmail.com>
2022-06-28 11:43:51 +0600

    Taildwin css scrollbar error fixed

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -7,7 +7,7 @@ export default function Posts() {
             id: 1,
             username: "Yearul",
             userimage: " https://islamyearul.xyz/wp-content/uploads/2022/04/Yearul-PP-2-Pic-2022.jpg",
-            img: "https://images.unsplash.com/photo-1656078747417-6922b13e8277?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            img: "https://images.unsplash.com/photo-1595567509863-bb5d02987bbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
             caption: "caption demo",
 
 
@@ -16,7 +16,7 @@ export default function Posts() {
             id: 1,
             username: "Yearul",
             userimage: " https://islamyearul.xyz/wp-content/uploads/2022/04/Yearul-PP-2-Pic-2022.jpg",
-            img: "https://images.unsplash.com/photo-1656078747417-6922b13e8277?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            img: "https://images.unsplash.com/photo-1595567509863-bb5d02987bbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
             caption: "caption demo",
 
 
